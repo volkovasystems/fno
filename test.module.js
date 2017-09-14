a8921ccf9a900784e26ec162f04b71dba787d07d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-09-14 16:11:57 +0800

    Added tests.

--- test.module.js
+++ test.module.js
@@ -45,13 +45,13 @@
 
 	@include:
 		{
-			"assert": "should",
+			"assert": "should/as-function",
 			"fno": "fno"
 		}
 	@end-include
 */
 
-const assert = require( "should" );
+const assert = require( "should/as-function" );
 
 //: @server:
 const fno = require( "./fno.js" );
@@ -84,6 +84,41 @@ describe( "fno", ( ) => {
 		} );
 	} );
 
+	describe( "`fno( function hello( ){ } ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof fno( function hello( ){ } ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`fno( function hello( ){ } ).toNumber( )`", ( ) => {
+		it( "should return NaN", ( ) => {
+			assert.equal( fno( function hello( ){ } ).toNumber( ).toString( ), "NaN" );
+		} );
+	} );
+
+	describe( "`fno( function hello( ){ } ).toBoolean( )`", ( ) => {
+		it( "should return boolean type", ( ) => {
+			assert.equal( typeof fno( function hello( ){ } ).toBoolean( ), "boolean" );
+		} );
+	} );
+
+	describe( "`no( function hello( ){ } ).toObject( )`", ( ) => {
+		it( "should return object type", ( ) => {
+			let descriptor = fno( function hello( ){ } ).toObject( );
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
+		} );
+	} );
+
 } );
 
 //: @end-server
@@ -104,6 +139,41 @@ describe( "fno", ( ) => {
 			assert.equal( data.constructor.name, "Procedure" );
 
 			assert.equal( data.valueOf( ), method );
+		} );
+	} );
+
+	describe( "`fno( function hello( ){ } ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof fno( function hello( ){ } ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`fno( function hello( ){ } ).toNumber( )`", ( ) => {
+		it( "should return NaN", ( ) => {
+			assert.equal( fno( function hello( ){ } ).toNumber( ).toString( ), "NaN" );
+		} );
+	} );
+
+	describe( "`fno( function hello( ){ } ).toBoolean( )`", ( ) => {
+		it( "should return boolean type", ( ) => {
+			assert.equal( typeof fno( function hello( ){ } ).toBoolean( ), "boolean" );
+		} );
+	} );
+
+	describe( "`no( function hello( ){ } ).toObject( )`", ( ) => {
+		it( "should return object type", ( ) => {
+			let descriptor = fno( function hello( ){ } ).toObject( );
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
 		} );
 	} );
 
@@ -128,6 +198,78 @@ describe( "fno", ( ) => {
 
 			assert.equal( browser.url( bridgeURL ).execute( ( ) => `${ fno( function hello( ){ } ).valueOf( ).name }` ).value, "hello" );
 
+		} );
+	} );
+
+	describe( "`fno( function hello( ){ } ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			//: @ignore:
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return typeof fno( function hello( ){ } ).toString( );
+				}
+
+			).value;
+			//: @end-ignore
+
+			assert.equal( result, "string" );
+		} );
+	} );
+
+	describe( "`fno( function hello( ){ } ).toNumber( )`", ( ) => {
+		it( "should return NaN", ( ) => {
+			//: @ignore:
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return fno( function hello( ){ } ).toNumber( ).toString( );
+				}
+
+			).value;
+			//: @end-ignore
+
+			assert.equal( result, "NaN" );
+		} );
+	} );
+
+	describe( "`fno( function hello( ){ } ).toBoolean( )`", ( ) => {
+		it( "should return boolean type", ( ) => {
+			//: @ignore:
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return typeof fno( function hello( ){ } ).toBoolean( );
+				}
+
+			).value;
+			//: @end-ignore
+
+			assert.equal( result, "boolean" );
+		} );
+	} );
+
+	describe( "`no( function hello( ){ } ).toObject( )`", ( ) => {
+		it( "should return object type", ( ) => {
+			//: @ignore:
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					let descriptor = fno( function hello( ){ } ).toObject( );
+
+					let test = typeof descriptor == "object" &&
+						"type" in descriptor == true &&
+						"name" in descriptor == true &&
+						"value" in descriptor == true &&
+						"format" in descriptor == true;
+
+					return test;
+				}
+
+			).value;
+			//: @end-ignore
+
+			assert.equal( result, true );
 		} );
 	} );
 
